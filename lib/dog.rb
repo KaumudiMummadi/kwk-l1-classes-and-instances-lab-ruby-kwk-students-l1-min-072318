f16e8a5fa1ef3e6e14b930a03b5f1ef293852b7d
KaumudiMummadi <kaumudi.mummadi@gmail.com>
2018-07-27 16:36:25 +0000

    automatically backed up by learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,13 +1,10 @@
 # dog.rb
 
 class Dog 
-  def initialize (name, name1, name2)
-    @name = name 
-    @name1 = name 
-    @name2 = name 
-  puts "My dogs names are #{@name}, #{@name1} and #{@name2}"
-  end 
+
 end 
 
-fido = Dog.new("fido", "snoopy", "lassie")
+Dog = fido.new 
+Dog = snoopy.new
+Dog = lassie.new
     
